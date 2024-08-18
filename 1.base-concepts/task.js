d0ad62fa36affe6d33f4edf07295156c820f17da
ailunaM <ajlunaravsanbekova@gmail.com>
2024-08-19 00:51:01 +0600

    Fixsed errors

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,28 +3,27 @@ function solveEquation(a, b, c) {
   let discriminantFormula = Math.pow(b, 2) - 4 * a * c;
   // console.log(discriminantFormula);
   let arr = [];
-  if (discriminantFormula < 0) {
-    return arr;
-  } else if (discriminantFormula === 0) {
+  if (discriminantFormula === 0) {
     const root = -b / (2 * a);
     arr.push(root);
-    return arr;
+    
   } else if (discriminantFormula > 0) {
     const root = Math.round((-b + Math.sqrt(b)) / (2 * a));
     const rootTwo = Math.round((-b - Math.sqrt(b)) / (2 * a));
     arr.push(root);
     arr.push(rootTwo);
-    return arr;
+    
   }
+
+  return arr
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let S = amount - contribution;
-  let n = countMonths;
-  let P = percent / 100 / n;
+  let amountReturnedBank = amount - contribution;
+  let countRate = percent / 100 / 12;
 
-  let payment = S * (P + P / (Math.pow(1 + P, n) - 1));
-  let totalAmount = payment * n;
+  let payment =  amountReturnedBank * (countRate + countRate / (Math.pow(1 + countRate, countMonths) - 1));
+  let totalAmount = payment * countMonths;
 
   return +parseFloat(totalAmount).toFixed(2);
 }
