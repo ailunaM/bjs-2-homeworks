96d1cc9bee19df6a3dc1a2647ce0d36b3261e584
ailunaM <ajlunaravsanbekova@gmail.com>
2024-08-18 14:55:52 +0600

    base-concepts/Decided task №1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
-"use strict"
+'use strict';
 function solveEquation(a, b, c) {
+  let discriminantFormula = Math.pow(b, 2) - 4 * a * c;
+  // console.log(discriminantFormula);
   let arr = [];
-  
-  return arr;
+  if (discriminantFormula < 0) {
+    return arr;
+  } else if (discriminantFormula === 0) {
+    const root = -b / (2 * a);
+    arr.push(root);
+    return arr;
+  } else if (discriminantFormula > 0) {
+    const roott = Math.round((-b + Math.sqrt(b)) / (2 * a));
+    const root = Math.round((-b - Math.sqrt(b)) / (2 * a));
+    arr.push(roott);
+    arr.push(root);
+    return arr;
+  }
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {}
