dd85e6e43aef28158870ac7c442bc2d335959832
ailunaM <ajlunaravsanbekova@gmail.com>
2024-08-26 23:19:48 +0600

    task №1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,6 +1,10 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
+new Student()
 
 Student.prototype.setSubject = function (subjectName) {
   
