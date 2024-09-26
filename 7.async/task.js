6df284ee8773a8eaa89ce0d49c12c89ac0e149b9
ailunaM <ajlunaravsanbekova@gmail.com>
2024-09-26 17:19:46 +0600

    task №1

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,12 +1,66 @@
 class AlarmClock {
   constructor() {
     this.alarmCollection = [];
-    this.intervalId = 0;
+    this.intervalId = null;
   }
 
+  // 14:50
   addClock(time, callback) {
     if (!time || !callback) {
       throw new Error('Отсутствуют обязательные аргументы');
     }
+    for (let i = 0; i < this.alarmCollection.length; i++) {
+      if (this.alarmCollection[i].time === time) {
+        console.warn('Уже присутствует звонок на это же время');
+      }
+    }
+
+    this.alarmCollection.push({
+      time: time,
+      callback: callback,
+      canCall: true,
+    });
+  }
+
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter((alarm) => alarm.time !== time);
+  }
+
+  getCurrentFormattedTime() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+
+    return `${hours}:${minutes}`;
+  }
+
+  start() {
+    if (this.intervalId !== null) {
+      return;
+    }
+    this.intervalId = setInterval(() => {
+      this.alarmCollection.forEach((alarm) => {
+        if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall === true) {
+          alarm.canCall = false;
+          alarm.callback();
+        }
+      });
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  resetAllCalls() {
+    this.alarmCollection.forEach((alarm) => {
+      alarm.canCall = true;
+    });
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
   }
 }
